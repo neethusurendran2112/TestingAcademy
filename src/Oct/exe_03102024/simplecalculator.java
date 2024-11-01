fc54ca697b5107f00ede3f51c707db6c56438307
Neethusurendran2112 <neethusurendran2112@gmail.com>
2024-11-01 16:08:21 +0530

    Commit and push

---
+++ src/Oct/exe_03102024/simplecalculator.java
@@ -0,0 +1,11 @@
+package Oct.exe_03102024;
+
+import java.sql.SQLOutput;
+
+public class simplecalculator {
+
+    public static void main(String[] args) {
+        System.out.println(3 * 3);
+    }
+
+}
