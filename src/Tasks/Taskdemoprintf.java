02e5c711b7d82f025343098ee67a79b33899a671
neethusurendran2112 <neethusurendran2112@gmail.com>
2026-03-04 12:51:04 +0530

    commit
    commit

--- src/Tasks/Taskdemoprintf.java
+++ src/Tasks/Taskdemoprintf.java
@@ -1,24 +1,19 @@
 package Tasks;
-
-<<<<<<<< HEAD:src/Tasks/Taskdemoprintf.java
 public class Taskdemoprintf {
-========
-public class Task0910demoprintf {
->>>>>>>> origin/https/github.com/neethusurendran2112/TestingAcademy/blob/master/src/Oct:src/Tasks/Task0910demoprintf.java
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         int a = 10;
         char b = 'a';
         String s1 = "my name is neethu";
         Boolean val = true;
         float f1 = 10;
         double d1 = 100;
-        System.out.printf("integer value is %d",a);
-        System.out.printf("Character value is %c",b);
-        System.out.printf("String value is %s",s1);
-        System.out.printf("Boolean value is %b",b);
-        System.out.printf("Float value is %f",f1);
-        System.out.printf("Double value is %f",d1);
+        System.out.printf("integer value is %d", a);
+        System.out.printf("Character value is %c", b);
+        System.out.printf("String value is %s", s1);
+        System.out.printf("Boolean value is %b", b);
+        System.out.printf("Float value is %f", f1);
+        System.out.printf("Double value is %f", d1);
 
     }
 }
--- src/Tasks/Wrapperc.java
+++ src/Tasks/Wrapperc.java
@@ -1,4 +1,14 @@
 package Tasks;
 
 public class Wrapperc {
+    public static void main(String[] args) {
+        String s = "My name is neethu";
+        Integer i = 34;
+
+        System.out.println(s+i);
+    }
+
+
+
+
 }
